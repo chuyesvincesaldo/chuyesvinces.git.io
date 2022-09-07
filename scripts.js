e7024354bf53e7affc6518dc7953246e0250368f
Aldochuyes <112560673+chuyesvincesaldo@users.noreply.github.com>
2022-09-07 17:13:10 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,48 @@
+//e - enter
+//o - ober
+//i - imes
+//a - ai
+//u - ufat
+
+function encriptar(){
+	var texto = document.getElementByid("input-texto").value.toLowerCase();
+		// i es para que revise en toda la linea mayusculas y minusculas
+		// g para que revise toda la oracion
+		// m es para que revise en varias lineas 
+	var txtEncriptar = texto.replace(/e/igm,"enter");
+	var txtEncriptar = txtEncriptar.replace(/o/igm,"ober");
+	var txtEncriptar = txtEncriptar.replace(/i/igm,"imes");
+	var txtEncriptar = txtEncriptar.replace(/a/igm,"ai");
+	var txtEncriptar = txtEncriptar.replace(/u/igm,"ufat");
+
+	document.getElementById("imgDer").style.display = "none"; 
+	document.getElementById("textor").style.display = "none"; 
+	document.getElementById("texto2").innerHTML = txtEncriptar;
+	document.getElementById("copiar").style.display = "show";
+	document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar(){
+	var texto = document.getElementById("input-texto").value.toLowerCase();
+		// i es para que revise en toda la linea mayusculas y minusculas
+		// g para que revise toda la oracion
+		// m es para que revise en varias lineas 
+	var txtEncriptar = texto.replace(/enter/igm,"e");
+	var txtEncriptar = txtEncriptar.replace(/ober/igm,"o");
+	var txtEncriptar = txtEncriptar.replace(/imes/igm,"i");
+	var txtEncriptar = txtEncriptar.replace(/ai/igm,"a");
+	var txtEncriptar = txtEncriptar.replace(/ufat/igm,"u");
+
+	document.getElementById("imgDer").style.display = "none"; 
+	document.getElementById("textor").style.display = "none"; 
+	document.getElementById("texto2").innerHTML = txtEncriptar;
+	document.getElementById("copiar").style.display = "show";
+	document.getElementById("copiar").style.display = "inherit";
+}
+
+function copy(){
+	var contenido = document.querySelector("texto2"):
+	contenido.Select();
+	document.execCommand("copy");
+	alert("Texto Copiado"); 
+}
